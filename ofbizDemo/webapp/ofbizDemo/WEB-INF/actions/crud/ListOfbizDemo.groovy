cc88161f4d53d88e2f0d20473eeb0e54adadd6ec
rahul pandey <rahulpandey8397136@gmail.com>
2022-02-08 15:58:57 +0530

    add a new directory name includes and create two files name PreBody.ftl and PostBody.ftl

--- ofbizDemo/webapp/ofbizDemo/WEB-INF/actions/crud/ListOfbizDemo.groovy
+++
@@ -1,4 +0,0 @@
-ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
-context.ofbizDemoTypes = ofbizDemoTypes;
-ofbizDemoList = delegator.findList("OfbizDemo", null, null, null, null, false);
-context.ofbizDemoList = ofbizDemoList;
